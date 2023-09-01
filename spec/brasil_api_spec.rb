d0d3a7639be60d368c50d77f6874505adef962d5
Marco Ferraz <marco.ferraz98@outlook.com>
2023-09-01 18:11:19 -0300

    initial commit

---
+++ spec/brasil_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BrasilApi do
+  it "has a version number" do
+    expect(BrasilApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
